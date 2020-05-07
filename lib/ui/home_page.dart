6f03c37eb12b4adc73f27ac77d25d1f3b8420a4d
ocostalucas <olucascosta98@gmail.com>
2020-05-07 00:26:33 -0300

    added order list

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -5,6 +5,8 @@ import 'package:url_launcher/url_launcher.dart';
 
 import 'package:phonebook/ui/contact_page.dart';
 
+enum OrderOptions {orderaz, orderza}
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -28,6 +30,21 @@ class _HomePageState extends State<HomePage> {
         title: Text("Contacts"),
         backgroundColor: Colors.red,
         centerTitle: true,
+        actions: <Widget>[
+          PopupMenuButton<OrderOptions>(
+            itemBuilder: (context) => <PopupMenuEntry<OrderOptions>>[
+              const PopupMenuItem<OrderOptions>(
+                child: Text("A-Z order"),
+                value: OrderOptions.orderaz,
+              ),
+              const PopupMenuItem<OrderOptions>(
+                child: Text("Z-A order"),
+                value: OrderOptions.orderza,
+              ) 
+            ],
+            onSelected: _orderList,
+          )
+        ],
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
@@ -177,4 +194,22 @@ class _HomePageState extends State<HomePage> {
       });
     });
   }
+
+  void _orderList(OrderOptions result){
+  switch(result){
+    case OrderOptions.orderaz:
+      contacts.sort((a, b){
+        return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+      }
+      );
+      break;
+    case OrderOptions.orderza:
+      contacts.sort((a, b){
+        return b.name.toLowerCase().compareTo(a.name.toLowerCase());
+      }
+      );
+      break;
+  }
+  setState((){});
+}
 }
